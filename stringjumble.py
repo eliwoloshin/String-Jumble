6f50edf40825a7eaf00db20c07d0a4a7f164a523
eliwoloshin <eliwoloshin@hanovernorwichschools.org>
2016-03-03 14:55:42 -0500

    Updated from Brython Server: 3/3/2016 2:55:41 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,5 +25,9 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+import string
+
 string = input("Please enter a string of text (the bigger the better): ")
+
+string[::-1]
 
